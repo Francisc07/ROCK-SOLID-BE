d5f80bab27836e27a0d031f48f347c1288150998
Francisco <44477215+Francisc07@users.noreply.github.com>
2024-04-25 12:54:40 -0600

    Update users.ts

--- src/app/routes/users.ts
+++ src/app/routes/users.ts
@@ -1,27 +1,37 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import { client } from './config/config';
-import { Document, WithId } from 'mongodb';
+import { Users } from '../models/Users';
 const dbName = process.env.DB_NAME;
 const collectionName = 'users';
 export const userRouter = express.Router();
 
-userRouter.get('/getUsers', async (req, res) => {
+userRouter.get('/getUsers', async (req: Request, res: Response) => {
   try {
     await client.connect();
     // Send a ping to confirm a successful connection
     await client.db('admin').command({ ping: 1 });
     const database = client.db(dbName);
     const collection = database.collection(collectionName);
-    const usuarios: WithId<Document>[] = await collection.find({}).toArray();
+    const usuarios: Users[] = (await collection.find({}).toArray()).map(
+      (doc) => ({
+        _id: doc._id.toHexString(),
+        name: doc.name,
+        email: doc.email,
+        password: doc.password,
+      })
+    );
 
     res.send(usuarios);
-    client.close();
-    // add a linebreak
   } catch (err) {
     console.error(
       `Something went wrong trying to find the documents: ${err}\n`
     );
     res.status(500).json({ message: 'Error al obtener usuarios' });
     client.close();
+  } finally {
+    if (client) {
+      console.error(`Cierra conn`);
+      await client.close();
+    }
   }
 });
